6c7ceb88a68862d614536f1d0c9808467bf53982
unknown <redyk654@gmail.com>
2022-10-30 20:44:30 +0100

    maj du 30/10/22 important: suite code patient volet assurance

--- gestion_assurance.php
+++ gestion_assurance.php
@@ -16,7 +16,7 @@
             $date_max = $_POST['date_max'].' 23:59:59';
             $statu = 'pending';
     
-            $req = $bdd->prepare("SELECT id_fac, id, patient, prix_total, assurance, type_assurance, date_heure FROM
+            $req = $bdd->prepare("SELECT id_fac, id, code_patient, patient, prix_total, assurance, type_assurance, date_heure FROM
                     facture_pharmacie WHERE statu = :statu AND assurance = :assurance AND (date_heure BETWEEN :date_min AND :date_max) ORDER BY date_heure") or die(print_r($bdd->errorInfo()));
             $req->execute(
                 array(
@@ -57,6 +57,7 @@
         $data = $req->fetchAll();
         echo json_encode($data);
     } else if (isset($_POST['id_fac']) AND isset($_POST['categorie'])) {
+        // mise à jour du statu des factures réglés
         if ($_POST['categorie'] == "pharmacie") {
             $req = $bdd->prepare("UPDATE facture_pharmacie SET statu = ? WHERE id = ?");
     
@@ -79,13 +80,14 @@
         }
     } else if (isset($_POST['id_facture']) AND isset($_POST['nom']) AND isset($_POST['assurance']) AND isset($_POST['assurance_type']) AND isset($_POST['periode']) AND isset($_POST['total']) AND isset($_POST['reste'])) {
             // Enregistrement de la facture
-        $req = $bdd->prepare("INSERT INTO facture_assurance(id_facture, nom, assurance, assurance_type, periode, total, reste, date_heure) 
-        VALUES(:id_facture, :nom, :assurance, :assurance_type, :periode, :total, :reste, NOW())");
+        $req = $bdd->prepare("INSERT INTO facture_assurance(id_facture, nom, code_patient, assurance, assurance_type, periode, total, reste, date_heure) 
+        VALUES(:id_facture, :nom, :code_patient, :assurance, :assurance_type, :periode, :total, :reste, NOW())");
     
         $req->execute(
             array(
                 'id_facture' => $_POST['id_facture'],
                 'nom' => $_POST['nom'],
+                'code_patient' => $_POST['code_patient'],
                 'assurance' => $_POST['assurance'],
                 'assurance_type' => $_POST['assurance_type'],
                 'periode' => $_POST['periode'],
