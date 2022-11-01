b47a8c49e3761d76e1e710480cd10533c25d0e19
unknown <redyk654@gmail.com>
2022-11-01 20:42:37 +0100

    section details ventes

---
+++ details_ventes.php
@@ -0,0 +1,53 @@
+<?php
+    header('Access-Control-Allow-Origin: *');
+
+    //Récupération des recettes à gérer
+
+    try {
+	    $bdd = new PDO('mysql:host=localhost;dbname=cma;charset=utf8', 'root', '');
+    }
+    catch (Exception $e) {
+        die('Erreur : ' . $e->getMessage());
+    }
+
+    if (isset($_POST['dateD']) AND isset($_POST['dateF'])) {
+
+        if (isset($_GET['vendeur'])) {
+
+            $debut = $_POST['dateD'];
+            $fin = $_POST['dateF'];
+            $vendeur = $_GET['vendeur'];
+            $statu = 'payé';
+            
+            $req = $bdd->prepare("SELECT *, DATE_FORMAT(date_vente, '%d/%m/%Y %Hh%imin%ss') as date_heure
+                                    FROM historique WHERE (date_vente BETWEEN :precedent AND :actuel) AND nom_vendeur = :vendeur AND status_vente = :statu") or die(print_r($bdd->errorInfo()));
+            $req->execute(
+                array(
+                    'precedent' => $debut,
+                    'actuel' => $fin,
+                    'vendeur' => $vendeur,
+                    'statu' => $statu,
+                )
+            );
+
+        } else {
+
+            $debut = $_POST['dateD'];
+            $fin = $_POST['dateF'];
+            $statu = 'payé';
+            
+            $req = $bdd->prepare("SELECT *, DATE_FORMAT(date_vente, '%d/%m/%Y %Hh%imin%ss') as date_heure
+                                    FROM historique WHERE date_vente BETWEEN :precedent AND :actuel AND status_vente = :statu");
+            $req->execute(
+                array(
+                    'precedent' => $debut,
+                    'actuel' => $fin,
+                    'statu' => $statu,
+                )
+            );
+        }
+
+        $data = $req->fetchAll();
+        echo json_encode($data);
+    }
+?>
